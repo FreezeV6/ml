933c519dcd98f7dbcfb13ae57ee5f6c5299913c5
FreezeV5 <tarabulakrzysztof@gmail.com>
2025-03-12 13:54:02 +0100

    add data and tweak functions

--- main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:15ecfb7555afaa793f4f319993009e65627c72624e4d056e5489c0377e30920f
-size 3350
---
+++ src/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41fe8512772da382eb7caa625b41f45b06e3a537c737d6a3183f5029f8d37d1e
+size 2861
