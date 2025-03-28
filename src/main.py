42c1eac65392f8f85c12ac5f57d075f1f4e61f5e
Krzysztof Tarabula <tarabulakrzysztof@gmail.com>
2025-03-28 18:24:53 +0100

    add information gain calculate function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41fe8512772da382eb7caa625b41f45b06e3a537c737d6a3183f5029f8d37d1e
-size 2861
+oid sha256:bd81de2fd5f3b3b3dd7504168710e234e7996555da22562a554195040fa5e318
+size 3536
