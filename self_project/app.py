0bf06280e32fc628e36bad3fc3dbecef88d78acc
Krzysztof Tarabula <tarabulakrzysztof@gmail.com>
2025-04-16 03:44:41 +0200

    almost final version

--- self_project/app.py
+++ self_project/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50a3c7c20edaeb30c52e291b0b87fde1debab8dd8dd508794cb03f2c15670030
-size 3249
+oid sha256:6e150c4b1bb66001b40114d1592354c3483bace4445b47b9d9268b262e984c5c
+size 2842
