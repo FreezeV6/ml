9dcc9a80e52378aef73ea4168686f200b3c3217b
FreezeV6 <krzysztof.tarabula@edu.uekat.pl>
2025-05-07 10:10:09 +0200

    final v1

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72461a259991b84c4e0ea47de6be3f1621ce26190e056232bcce751cb782b206
-size 7059
+oid sha256:1fd68229f872fa26c2f04548db9030b130e6e90a0576f9bdcb344aed157d1bfa
+size 7016
